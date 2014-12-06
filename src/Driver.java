1c29f0f5cb9af56b8fb5fa4d08ee15b71683866c
Matt <navx2810@gmail.com>
2014-12-06 17:50:39 -0500

    removed references to the WDGraph and EdgeNodeBuilder as that is handled with PathManager

--- src/Driver.java
+++ src/Driver.java
@@ -1,20 +1,13 @@
 import java.io.File;
 
-import util.EdgeNodeBuilder;
-import entities.Graph;
-import graphs.WDGraph;
-import graphs.WGraph;
-
 
 public class Driver {
 
 	public static void main(String[] args) 
 	{
-		WDGraph graph = new WDGraph();
 		
-		EdgeNodeBuilder edgeNodeBuilder = new EdgeNodeBuilder(graph);
-		
-		edgeNodeBuilder.readFromFile(new File("final.data"));
+		PathManager manager = new PathManager(new File("final.data"));
+		manager.findPaths();
 		
 //		JFileChooser chooser = new JFileChooser();
 //		
