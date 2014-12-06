5ddf4e66663fab4c27d33395ef19f72b31c0724e
Matt <navx2810@gmail.com>
2014-12-06 15:58:32 -0500

    Cleaned up EdgeNodeBuilder. . . - removed the if statements to check to see if line equals a char. This was not how the data was structured - added a method to read through the line to look for whitespaces or ',' which indicate that it is a data entry line, not a state switching line - changed the ArrayList<Node> variable nodes to point to the ArrayList<Node> inside of the graph

--- src/util/EdgeNodeBuilder.java
+++ src/util/EdgeNodeBuilder.java
@@ -7,8 +7,8 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-import entities.Graph;
 import entities.Node;
+import graphs.WDGraph;
 
 
 /*
@@ -20,12 +20,12 @@ public class EdgeNodeBuilder
 	
 	private ArrayList<Node> nodes;
 	private Queue<Node> startNodes, endNodes;
-	private Graph graph;
+	private WDGraph graph;
 	
-	public EdgeNodeBuilder(Graph graph) 
+	public EdgeNodeBuilder(WDGraph graph) 
 	{
 		this.graph = graph;
-		nodes = new ArrayList<Node>();
+		nodes = graph.nodes;
 		startNodes = new LinkedList<Node>();
 		endNodes = new LinkedList<Node>();
 	}
@@ -37,19 +37,30 @@ public class EdgeNodeBuilder
 			Scanner scanner = new Scanner(file);
 			
 			@SuppressWarnings("unused")
-			int numOfVertices = Integer.parseInt(scanner.nextLine());
+			int numOfVertices = Integer.parseInt(scanner.nextLine());		// Calculated through the size of the nodeList
 			
-			state readingState = null;
+			state readingState = state.VERTICIES;
 			
 			while ( scanner.hasNext() )
 			{
 				String line = scanner.nextLine();
-				if ( line.equals("V") )
-					readingState = state.VERTICIES;
-				else if ( line.equals("E") )
-					readingState = state.EDGES;
-				else if ( line.equals("N") )
-					readingState = state.CASES;
+				
+				if( isAnIdentifier(line) )
+				{
+					switch (readingState) 
+					{
+					case VERTICIES:
+						readingState = state.EDGES; break;
+					case EDGES:
+						readingState = state.CASES; break;
+					case CASES:
+						break;
+					default:
+						readingState = state.VERTICIES; break;
+					}
+					
+				line = scanner.nextLine();
+				}
 				
 				switch (readingState) 
 				{
@@ -59,32 +70,37 @@ public class EdgeNodeBuilder
 				case EDGES:
 					readEdge(line);
 					break;
-				case CASES:
-					readCase(line);
-					break;
 				default:
+					readCase(line);
 					break;
 				}
 			}
 			
 			scanner.close();
-			this.graph.nodes = nodes;
-			this.graph.endNodes = endNodes;
-			this.graph.startNodes = startNodes;
-			
+			graph.starts = startNodes;
+			graph.ends = endNodes;
+			System.out.println("done");
 			
 		} catch (FileNotFoundException e) { e.printStackTrace(); }
 		
 		
 	}
 	
+	private boolean isAnIdentifier(String line) 
+	{
+		for( char c : line.toCharArray() )
+			if ( Character.isWhitespace(c) || c == ',' )
+				return false;
+		return true;
+	}
+
 	private void readNode(String line)
 	{
 		String[] container = line.split(",");
 		int id = Integer.parseInt(container[0]);
-		int longitude = Integer.parseInt(container[1]);
-		int latitude = Integer.parseInt(container[2]);
-		int height  = Integer.parseInt(container[3]);
+		double longitude = Double.parseDouble(container[1]);
+		double latitude = Double.parseDouble(container[2]);
+		double height  = Double.parseDouble(container[3]);
 		
 		nodes.add(new Node(id, container[3], longitude, latitude, height));
 	}
@@ -100,11 +116,11 @@ public class EdgeNodeBuilder
 		Node b = nodes.get(v_id2);
 		
 		if ( edge_state == 1)
-			a.addEdgeTo(b);
+			graph.addEdge(a, b);
 		else
 		{
-			a.addEdgeTo(b);
-			b.addEdgeTo(a);
+			graph.addEdge(a, b);
+			graph.addEdge(b, a);
 		}
 	}
 	
