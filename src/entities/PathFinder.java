bbd12f059166eea673bb16856443c86df2400aa3
Matt <navx2810@gmail.com>
2014-12-07 04:38:04 -0500

    removed un-used import statements

--- src/entities/PathFinder.java
+++ src/entities/PathFinder.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
 import java.util.Queue;
 
 import graphs.WDGraph;
