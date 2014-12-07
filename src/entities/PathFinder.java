51e7a32489c3c272273749b842ffb0490d40a6ee
navx2810 <navx2810@gmail.com>
2014-12-07 03:58:08 -0500

    removed the commented Queues and fleshed out the Dijkstra's Algorithm and the printPath method

--- src/entities/PathFinder.java
+++ src/entities/PathFinder.java
@@ -1,5 +1,10 @@
 package entities;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 import graphs.WDGraph;
 
@@ -9,7 +14,6 @@ public class PathFinder {
 	HashMap<Node, Boolean> visited;
 	WDGraph graph;
 	Node origin, destination;
-//	Queue<Node> starts, ends; 	//TODO: place this in a PathManager class
 	
 	public PathFinder(WDGraph graph, Node origin, Node destination) {
 		this.graph = graph;
@@ -25,14 +29,41 @@ public class PathFinder {
 	public void findBestPath()
 	{
 		visited.put(origin, true);
+		Queue<Node> nodeQueue = new LinkedList<Node>();
+		HashMap<Node, Node> previousMap = new HashMap<Node, Node>();
+		
+		origin.minDistance = 0.0;
+		
+		nodeQueue.add(origin);
+		
+		while ( !nodeQueue.isEmpty() )
+		{
+			Node node = nodeQueue.poll();
+			
+			for(Edge edge : node.edges)
+			{
+				Node target = edge.target;
+				double distanceThrough = node.minDistance + edge.weight;
+				if ( distanceThrough < target.minDistance)
+				{
+					nodeQueue.remove( target );
+					target.minDistance = distanceThrough;
+					previousMap.put(target, node);
+					nodeQueue.add( target );
+				}
+			}
+		}	
 		// Insert Dijkstra's Algorithm here
 		
-		printPath();
+		printPath(previousMap);
 	}
 	
-	private void printPath()
+	private void printPath(HashMap<Node,Node> previousMap)
 	{
-		// Print path here
+		ArrayList<Node> path = new ArrayList<Node>();
+		for( Node target = destination; target != null; target = previousMap.get(target))
+			path.add(target);
+		Collections.reverse(path);
 		System.out.println("\nbest path\n");
 	}
 	
