45e1c48f1ed0555211c3f948cec9bda3e766f5a3
Matt <navx2810@gmail.com>
2014-12-07 22:27:47 -0500

    remove references to calculateDistance as that is handled in the EdgeNodeBuilder

--- src/graphs/WDGraph.java
+++ src/graphs/WDGraph.java
@@ -14,31 +14,11 @@ public class WDGraph
 		nodes = new ArrayList<Node>();
 	}
 		
-	public void addEdge(Node origin, Node destination)
+	public void addEdge(Node origin, Node destination, Double weight)
 	{
-		origin.addEdgeTo(destination, calculateDistances(origin, destination));
+		origin.addEdgeTo(destination, weight);
 	}
 	
-	protected double calculateDistances(Node origin, Node destination)
-	{
-		double lat1 = origin.latitude;
-		double lng1 = origin.longitude;
-		double lat2 = destination.latitude;
-		double lng2 = destination.longitude;
-		
-		double earthRadius = 3958.75;	// Converts to Miles
-	    double dLat = Math.toRadians(lat2-lat1);
-	    double dLng = Math.toRadians(lng2-lng1);
-	    double sindLat = Math.sin(dLat / 2);
-	    double sindLng = Math.sin(dLng / 2);
-	    double a = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)
-	            * Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2));
-	    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-	    double dist = earthRadius * c;
-
-	    return dist;
-	}
-
 	public void setEntryAndDestination(Node entry, Node destination) 
 	{
 		this.entry = entry;
