4272f4c853f94be764b368369b2ec0d936860b12
Matt <navx2810@gmail.com>
2014-12-07 22:49:57 -0500

    added the C class to the driver

--- src/CIT360FinalProject.java
+++ src/CIT360FinalProject.java
@@ -1,10 +1,14 @@
 import java.io.File;
 
+import sections.C;
+
 
 public class CIT360FinalProject {
 
 	public static void main(String[] args) 
 	{
+		
+		C c = new C();
 		
 		PathManager manager = new PathManager(new File("final.data"));
 		manager.findPaths();
